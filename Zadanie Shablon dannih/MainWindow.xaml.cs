338c2fe49cf9ae3c6ec9e20f8bcafb3165f80d29
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-12-29 09:43:22 +0300

    Add files via upload

---
+++ Zadanie Shablon dannih/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace Zadanie_Shablon_dannih
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> productes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            productes = new List<Product>();
+            productes.Add(new Product()
+            {
+                Nazvanie = "сыр",
+                Cena = 400,
+                Izobragenie = "Data/cheese.png",
+                Category = Categories.Eda,
+            });
+            productes.Add(new Product()
+            {
+                Nazvanie = "молоко",
+                Cena = 80,
+                Izobragenie = "Data/milk.jpg",
+                Category = Categories.Eda,
+            });
+            productes.Add(new Product()
+            {
+                Nazvanie = "телефон",
+                Cena = 7000,
+                Izobragenie = "Data/phone.png",
+                Category = Categories.Technika,
+            });
+            productes.Add(new Product()
+            {
+                Nazvanie = "телевизор",
+                Cena = 15000,
+                Izobragenie = "Data/televizor.jpg",
+                Category = Categories.Technika,
+            });
+            lstBox.ItemsSource = productes;
+            
+        }
+
+        
+    }
+}
---
+++ Zadanie Shablon dannih/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_Shablon_dannih
+{
+    public enum Categories
+    {
+        Eda,
+        Technika
+    }
+    public class Product
+    {
+        public string Nazvanie { get; set; }
+        public int Cena { get; set; }
+        public string Izobragenie { get; set; }
+        public Categories Category { get; set; }
+        
+    }
+}
